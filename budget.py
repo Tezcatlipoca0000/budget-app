f731264989f216ab3a9c47612764fdca30807269
Tezcatlipoca <tezcatlipoca0000@gmail.com>
2022-11-18 11:42:56 -0600

    My solution

--- budget.py
+++ budget.py
@@ -4,19 +4,23 @@ class Category:
 
     def __init__(self, n) :
         self.name = n
+        self.ledger = []
 
     def deposit(self, amount, desc = "") :
-        self.ledger.append({"amount": amount, "description": desc})
+        self.ledger.append({"amount": float(amount), "description": desc})
     
     def withdraw(self, amount, desc = "") :
         if (self.check_funds(amount)) :
-            self.ledger.append({"amount": -abs(amount), "description": desc})
+            self.ledger.append({"amount": float(-abs(amount)), "description": desc})
+            return True
+        else :
+            return False
 
     def get_balance(self) :
         balance = 0
         for item in self.ledger :
-            balance = balance + item['amount']
-        return balance
+            balance = balance + float(item['amount'])
+        return float(balance)
 
     def transfer(self, amount, cat) :
         if (self.check_funds(amount)) :
@@ -29,7 +33,7 @@ class Category:
     def check_funds(self, amount) :
         funds = 0
         for item in self.ledger :
-            funds = funds + item['amount']
+            funds = funds + float(item['amount'])
         if amount > funds :
             return False
         else :
@@ -38,6 +42,9 @@ class Category:
     def __str__(self) :
         res = ''
         len_star = int((30 - len(self.name)) / 2)
+        total = 0
+        for item in self.ledger :
+            total = total + item['amount']
         for i in range(len_star) :
             res = res + '*'
         res = res + self.name
@@ -46,7 +53,7 @@ class Category:
         res = res + '\n'
         for item in self.ledger :
             desc_len = len(item['description'])
-            amount_len = len(str(item['amount']))
+            amount_len = len(f'{item["amount"]:.2f}')
             if desc_len < 23 :
                 res = res + item['description']
                 for i in range(23 - desc_len) :
@@ -57,11 +64,12 @@ class Category:
             if amount_len < 7 :
                 for i in range(7 - amount_len) :
                     res = res + ' '
-                res = res + str(item['amount'])
+                res = res + f'{item["amount"]:.2f}'
             else :
                 for i in range(7) :
-                    res = res + str(item['amount'])[i]
+                    res = res + f'{item["amount"]:.2f}'[i]
             res = res + '\n'
+        res = res + f'Total: {total}'
 
             
         return res
